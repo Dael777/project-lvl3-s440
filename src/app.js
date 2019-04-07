0508a46ec3b62e45b8c6dd0242258a7bc2a19fa7
Dael777 <dael85@gmail.com>
2019-04-07 19:06:44 +0300

    step 2 fixes (attempt 4)

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import { watch } from 'melanke-watchjs';
 import validator from 'validator';
 import axios from 'axios';
 import i18next from 'i18next';
+import $ from 'jquery';
+import _ from 'lodash';
+import corsProxy from './corsproxy';
 
 export default () => {
   i18next.init({
@@ -22,43 +25,43 @@ export default () => {
 
   const state = {
     newFeedUrl: null,
-    newFeed: {
-      title: null,
-      description: null,
-      articles: [],
-    },
+    newFeed: {},
     updatedFeedUrl: null,
     updatedFeedArticles: null,
     formStatus: 'resting',
-    feedNumber: 0,
     addedFeeds: [],
     error: null,
+    modal: null,
   };
-  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
   const addFeedInput = document.querySelector('#feed-url');
   const errorShow = addFeedInput.nextElementSibling;
   const addFeedForm = document.querySelector('#add-feed-form');
   const rssFeedsContainer = document.querySelector('#rss-feeds');
   const formSpinner = document.querySelector('#form-spinner');
   const btnSubmit = document.querySelector('#btn-submit');
+  const modalBody = document.querySelector('#modal-body');
 
-  const parseRss = ({ data }) => {
+  const parseRss = (data) => {
     const parser = new DOMParser();
     const rss = parser.parseFromString(data, 'application/xml');
     if (rss.documentElement.tagName !== 'rss') {
       return false;
     }
-    const feedTitle = rss.getElementsByTagName('title')[0].textContent;
-    const feedDescription = rss.getElementsByTagName('description')[0].textContent;
+    const feedTitle = rss.querySelector('title').textContent;
+    const feedDescription = rss.querySelector('description').textContent;
     const getFeedArticles = rss.querySelectorAll('item');
     const feedArticles = [...getFeedArticles].map((getFeedArticle) => {
-      const feedArticle = new Map();
       const articleTitle = getFeedArticle.children[0].textContent;
       const articleDescription = getFeedArticle.children[1].textContent;
       const articleLink = getFeedArticle.children[2].textContent;
-      feedArticle.set('title', articleTitle);
-      feedArticle.set('description', articleDescription);
-      feedArticle.set('link', articleLink);
+      const articleId = getFeedArticle.children[3].textContent;
+      const feedArticle = {
+        id: articleId,
+        title: articleTitle,
+        description: articleDescription,
+        link: articleLink,
+      };
       return feedArticle;
     });
     return {
@@ -69,22 +72,9 @@ export default () => {
   };
 
   const createHtmlItems = items => items.map(
-    (item, index) => `<li class="mb-1">
-      <a href="${item.get('link')}">${item.get('title')}</a>
-      <button type="button" class="btn btn-info" data-target="#item-${state.feedNumber}-${index}" data-toggle="modal">Description</button>
-      <div class="modal fade" id="item-${state.feedNumber}-${index}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Article description</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">${item.get('description')}</div>
-         </div>
-        </div>
-      </div>
+    item => `<li class="mb-1">
+      <a href="${item.link}">${item.title}</a>
+      <button type="button" class="btn btn-info" data-target="#exampleModal" data-id="${item.id}" data-toggle="modal">Description</button>
       </li>`,
   ).join('');
 
@@ -92,14 +82,14 @@ export default () => {
     setTimeout(() => {
       axios.get(`${corsProxy}${feedUrl}`)
         .then((response) => {
-          const updatedFeed = parseRss(response);
+          const updatedFeed = parseRss(response.data);
           state.updatedFeedUrl = feedUrl;
           state.updatedFeedArticles = updatedFeed.feedArticles;
         })
         .finally(() => {
           updateArticles(feedUrl);
         });
-    }, 5000);
+    }, 2000);
   };
 
   const showError = (errorKey) => {
@@ -151,12 +141,19 @@ export default () => {
     const updatedFeed = document.querySelector(`[data-feed="${state.updatedFeedUrl}"]`);
     const currentArticles = updatedFeed.querySelectorAll('li a');
     const currentLinks = [...currentArticles].map(currentArticle => currentArticle.href);
-    const newArticles = state.updatedFeedArticles.filter(
-      updatedArticle => !currentLinks.includes(updatedArticle.get('link')),
-    );
+    const newArticles = _.filter(state.updatedFeedArticles,
+      article => !currentLinks.includes(article.link));
+    if (newArticles.length > 0) {
+      state.addedFeeds = state.addedFeeds.concat(newArticles);
+    }
     const currentUl = updatedFeed.querySelector('ul');
     const newItems = createHtmlItems(newArticles);
     currentUl.insertAdjacentHTML('beforeEnd', newItems);
+  });
+
+  watch(state, 'modal', () => {
+    const currentArticleDescription = state.addedFeeds.find(el => el.id === state.modal);
+    $(modalBody).text(currentArticleDescription.description);
   });
 
   const inputValidate = () => {
@@ -186,18 +183,19 @@ export default () => {
     axios.get(`${corsProxy}${url}`)
       .then((response) => {
         state.formStatus = 'resting';
-        const newFeed = parseRss(response);
+        const newFeed = parseRss(response.data);
         if (!newFeed) {
           state.error = 'noRss';
           return;
         }
         state.newFeedUrl = url;
-        state.newFeed.title = newFeed.feedTitle;
-        state.newFeed.description = newFeed.feedDescription;
-        state.newFeed.articles = newFeed.feedArticles;
-        state.feedNumber += 1;
+        state.newFeed = {
+          title: newFeed.feedTitle,
+          description: newFeed.feedDescription,
+          articles: newFeed.feedArticles,
+        };
         state.error = null;
-        state.addedFeeds.push(url);
+        state.addedFeeds = state.addedFeeds.concat(newFeed.feedArticles);
       })
       .catch((error) => {
         state.formStatus = 'resting';
@@ -205,4 +203,9 @@ export default () => {
       });
   };
   addFeedForm.addEventListener('submit', addNewFeed);
+
+  $(document).on('show.bs.modal', '.modal', (e) => {
+    const currentArticleId = $(e.relatedTarget).attr('data-id');
+    state.modal = currentArticleId;
+  });
 };
